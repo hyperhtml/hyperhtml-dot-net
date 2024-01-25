73edeab769ea8f2a6990c352d76546a527070091
Bryce Carr <bryce@Bryces-MBP.localdomain>
2024-01-25 10:04:46 -0500

    Basic site

---
+++ app/pages/_app.tsx
@@ -0,0 +1,7 @@
+import type { AppProps } from 'next/app'
+import { config } from '@fortawesome/fontawesome-svg-core'
+config.autoAddCss = false
+
+export default function MyApp({ Component, pageProps }: AppProps) {
+    return <Component {...pageProps} />
+}
